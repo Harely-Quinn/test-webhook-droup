67f49cdb2318f71f22a0987d1261438ea7490ed5
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-13 16:37:38 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,6 +1,5 @@
 <?php
 file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query([
   'chat_id' => $chatId,
-  'text' => 'Hello!',
-  , // <--- This is a syntax error. Imagine that we did it by mistake.
+  'text' => 'Hello!', // <--- This is a syntax error. Imagine that we did it by mistake.
 ]));
