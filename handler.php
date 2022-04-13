a59ef784d5cf9e3b8e70a164f86756aa1c2e6f80
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-04-13 16:19:41 +0300

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,5 +1,2 @@
 <?php
-file_get_contents("https://api.telegram.org/bot1723005657:AAFxH-es1F5tWc6_ZQaMqVmpH1U5tMtVGkE/sendMessage?" . http_build_query([
-  'chat_id' => 1136071279,
-  'text' => 'Hello!', // <--- This is a syntax error. Imagine that we did it by mistake.
-]));
+file_get_contents("https://api.telegram.org/bot1723005657:AAFxH-es1F5tWc6_ZQaMqVmpH1U5tMtVGkE/sendMessage?chat_id=-1001658965255&disable_web_page_preview=True&text=\%23\Hello%20\%23\Text&parse_mode=MarkdownV2");
